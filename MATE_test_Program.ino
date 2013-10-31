e062bef8e68a125c328dad982e63478ff52fc877
ArduinoBoy <BJCandari@CDesign-MacBook-Pro.local>
2013-10-30 17:04:45 -1000

    edits

--- MATE_test_Program.ino
+++ MATE_test_Program.ino
@@ -25,19 +25,91 @@ digitalWrite(9,HIGH);
 
 
 void loop() {
- one();
-
- 
- 
+  one();
+  delay(1000);
+  two();
+  delay(1000); 
+  three();
+  delay(1000);
+  four();
+  delay(1000);
+  five();
+  delay(1000);
+  six();
+  delay(1000);
+  
+  
+  
 }
 
-
+//One
 void one(void) {
   digitalWrite(2,LOW);
+  digitalWrite(3,HIGH);
+  digitalWrite(4,HIGH);
+  digitalWrite(5,HIGH);
+  digitalWrite(6,HIGH);
+  digitalWrite(7,HIGH);
+  digitalWrite(8,HIGH);
+  digitalWrite(9,LOW);
+}
+
+//Two
+void two(void) {
+  digitalWrite(2,HIGH);
   digitalWrite(3,LOW);
   digitalWrite(4,LOW);
   digitalWrite(5,LOW);
-  digitalWrite(6,LOW);
+  digitalWrite(6,HIGH);
+  digitalWrite(7,HIGH);
+  digitalWrite(8,LOW);
+  digitalWrite(9,LOW);
+}
+
+//Three
+void three(void) {
+  digitalWrite(2,HIGH);
+  digitalWrite(3,LOW);
+  digitalWrite(4,LOW);
+  digitalWrite(5,LOW);
+  
+  digitalWrite(6,HIGH);
+  digitalWrite(7,LOW);
+  digitalWrite(8,LOW);
+  digitalWrite(9,HIGH);
+}
+
+//Four
+void four(void) {
+  digitalWrite(2,LOW);
+  digitalWrite(3,LOW);
+  digitalWrite(4,HIGH);
+  digitalWrite(5,LOW);
+  digitalWrite(6,HIGH);
+  digitalWrite(7,LOW);
+  digitalWrite(8,HIGH);
+  digitalWrite(9,HIGH);
+}
+
+//Five
+void five(void) {
+  digitalWrite(2,LOW);
+  digitalWrite(3,LOW);
+  digitalWrite(4,LOW);
+  digitalWrite(5,HIGH);
+  digitalWrite(6,HIGH);
+  digitalWrite(7,LOW);
+  digitalWrite(8,LOW);
+  digitalWrite(9,HIGH);
+}
+ 
+//Six
+void six(void) {
+  digitalWrite(2,LOW);
+  digitalWrite(3,LOW);
+  digitalWrite(4,LOW);
+  digitalWrite(5,HIGH);
+  digitalWrite(6,HIGH);
   digitalWrite(7,LOW);
   digitalWrite(8,LOW);
   digitalWrite(9,LOW);
